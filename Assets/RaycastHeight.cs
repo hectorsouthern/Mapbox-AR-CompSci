1b11ad73b94c9a2c9a6e62fad0f1dab677b5b93d
ProfHorse <htjsouthern@gmail.com>
2018-02-01 14:37:36 +0000

    Raycasting

---
+++ Assets/RaycastHeight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastHeight : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 origin = this.gameObject.transform.position;
+		origin = new Vector3(origin.x, origin.y + 1000, origin.z);
+		RaycastHit hit;
+		if (Physics.Raycast(origin, Vector3.down, out hit, Mathf.Infinity, 1 << 9)){
+			Debug.Log(hit.transform.position);
+			Debug.Log(hit.collider.gameObject.name);
+			this.gameObject.transform.position = hit.transform.position;
+		}
+	}
+}
