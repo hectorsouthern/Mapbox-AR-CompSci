6bd7eb364b2da8dc15f321ff8a04c02931a95c74
ProfHorse <htjsouthern@gmail.com>
2018-02-17 17:28:56 +0000

    Prog

---
+++ Assets/ChangeHeight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityARInterface;
+using UnityEngine;
+
+public class ChangeHeight : MonoBehaviour {
+
+	public static Action<BoundedPlane> planeAdded;
+
+	// Use this for initialization
+	void Start () {
+		planeAdded += getPlaneAdded;
+	}
+	
+	// Update is called once per frame
+	void getPlaneAdded (BoundedPlane plane) {
+		Debug.Log(plane.height);
+	}
+}
--- Assets/RaycastHeight.cs
+++ Assets/RaycastHeight.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mapbox.Unity.Map;
 
 public class RaycastHeight : MonoBehaviour {
-	
-	// Update is called once per frame
-	void Update () {
-		Vector3 origin = this.gameObject.transform.position;
-		origin = new Vector3(origin.x, origin.y + 1000, origin.z);
-		RaycastHit hit;
-		if (Physics.Raycast(origin, Vector3.down, out hit, Mathf.Infinity, 1 << 9)){
-			Debug.Log(hit.transform.position);
-			Debug.Log(hit.collider.gameObject.name);
-			this.gameObject.transform.position = hit.transform.position;
+	public Transform _MainCamera;
+	public float _heightAboveGround = 1;
+
+	private void Start()
+	{
+		//Default to moving the object itself.
+		if( _MainCamera == null )
+		{
+			_MainCamera = transform;
 		}
+	}
+
+	void Update () {
+
+		//TODO: Probably don't need to do this *every* frame.
+
+		RaycastHit hit;
+		//Simulate a ray from 1000m above the user to find out how far above/below the ground they are.
+		int layerMask = (1 << LayerMask.NameToLayer("Map")); //Only intersect with the "map" layer.
+		if (Physics.Raycast(this.transform.position + new Vector3(0, 1000, 0), Vector3.down, out hit, Mathf.Infinity, layerMask))
+		{
+			float distanceAboveGround = hit.distance - 1000;
+			if (distanceAboveGround < _heightAboveGround*0.9 || distanceAboveGround > _heightAboveGround*1.1 || Math.Abs(distanceAboveGround - _heightAboveGround) > 1)
+			{
+				//Debug.Log(name+" is " + distanceAboveGround + "m above the ground. Moving "+ _thingToMove.name + "...");
+				_MainCamera.transform.localPosition = new Vector3(_MainCamera.transform.localPosition.x, _MainCamera.transform.localPosition.y - distanceAboveGround + _heightAboveGround, _MainCamera.transform.localPosition.z);
+			}
+		}
+	}
+
+	public void setHeightAboveGround( float height)
+	{
+		_heightAboveGround = height;
 	}
 }
